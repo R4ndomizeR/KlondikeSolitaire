d1b45b7d9b06775f33843fd5333095c4a5042405
R4ndomizeR <krainev106@gmail.com>
2022-04-23 14:56:40 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:R4ndomizeR/KlondikeSolitaire.git main:gh-pages
+git push -f git@github.com:R4ndomizeR/KlondikeSolitaire.git origin:gh-pages
 
 cd -
